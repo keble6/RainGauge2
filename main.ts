589f336beef77503a3e142d98f157abf796a0b20
Rob Wilson <rob@rwilson.org.uk>
2021-03-23 12:42:51 +0000

    Simple control loop

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
+function doTare () {
+    serial.writeLine("Doing tare")
+    tareActive = 1
+    HX711.power_up()
+    HX711.tare(10)
+    HX711.power_down()
+    tareActive = 0
+}
 function showWeight () {
     rawWeight = HX711.get_units(20)
     rawWeightx10 = rawWeight * 10
     weight = Math.round(rawWeightx10) / 10
     serial.writeLine("" + weight + "g")
+}
+function pumpControl (pumpState: number) {
+    pins.digitalWritePin(DigitalPin.P8, pumpState)
 }
 // Button A => Pump toggle
 input.onButtonPressed(Button.A, function () {
@@ -11,28 +22,22 @@ input.onButtonPressed(Button.A, function () {
     } else {
         pumpState = 0
     }
-    pins.digitalWritePin(DigitalPin.P8, pumpState)
+    pumpControl(pumpState)
 })
 // Button B => Tare
 input.onButtonPressed(Button.B, function () {
-    tareActive = 1
-    HX711.power_up()
-    HX711.tare(10)
-    HX711.power_down()
-    tareActive = 0
+    doTare()
 })
+let pumpState = 0
 let weight = 0
 let rawWeightx10 = 0
 let rawWeight = 0
-let pumpState = 0
 let tareActive = 0
-// HX711 needs a pull-down
-pins.setPull(DigitalPin.P1, PinPullMode.PullDown)
-pins.setPull(DigitalPin.P2, PinPullMode.PullDown)
+let weightLimit = 350
 tareActive = 0
-// Set pump to off
-pumpState = 0
-pins.digitalWritePin(DigitalPin.P8, pumpState)
+let pumpTime = 10000
+pumpControl(0)
+doTare()
 HX711.SetPIN_DOUT(DigitalPin.P1)
 HX711.SetPIN_SCK(DigitalPin.P2)
 HX711.begin()
@@ -46,6 +51,19 @@ basic.forever(function () {
         HX711.power_up()
         showWeight()
         HX711.power_down()
+        if (weight > weightLimit) {
+            serial.writeLine("Pump ON")
+            pumpControl(1)
+            // Delay of ~1-2s is important
+            basic.pause(pumpTime)
+            pumpControl(0)
+            serial.writeLine("Pump OFF")
+            basic.pause(10000)
+            serial.writeLine("Tare active")
+            doTare()
+            serial.writeLine("Resume weighing")
+        }
     }
+    // Delay of ~1-2s is important
     basic.pause(2000)
 })
